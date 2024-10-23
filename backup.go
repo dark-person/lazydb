e5750f4ff52ad58bda4a6315b4ffa188651b7bf9
dark-person <66205094+dark-person@users.noreply.github.com>
2024-10-23 23:26:27 +0800

    Add `BackupTo()` func & modify auto backup logic

--- backup.go
+++ backup.go
@@ -9,30 +9,27 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 )
 
-// Create a backup of current database.
+// Create a backup of current database to given path.
 //
-// If backup directory is empty string, then no backup will be created.
-func (l *LazyDB) createBackup() (dest string, err error) {
+// This function will ignore backup directory setting.
+func (l *LazyDB) BackupTo(dest string) (err error) {
+	// Create file to prevent directory not existing
+	err = createDbFile(dest)
+	if err != nil {
+		return err
+	}
+
+	err = copyFile(l.dbPath, dest)
+	return err
+}
+
+// Start auto backup process. If version is latest (i.e. no need to update), then no auto backup will be performed.
+func (l *LazyDB) autoBackup(m *migrate.Migrate) (dest string, err error) {
 	// Prevent backup directory is empty string
 	if l.backupDir == "" {
 		return "", nil // Consider as graceful return
 	}
 
-	// Prepare database name
-	dest = defaultBackupPath(l.dbPath, l.backupDir)
-
-	// Create file to prevent directory not existing
-	err = createDbFile(dest)
-	if err != nil {
-		return dest, err
-	}
-
-	err = copyFile(l.dbPath, dest)
-	return dest, err
-}
-
-// Start auto backup process. If version is latest (i.e. no need to update), then no auto backup will be performed.
-func (l *LazyDB) autoBackup(m *migrate.Migrate) (dest string, err error) {
 	if m == nil {
 		return "", fmt.Errorf("migrate instance is nil")
 	}
@@ -54,11 +51,21 @@ func (l *LazyDB) autoBackup(m *migrate.Migrate) (dest string, err error) {
 		return "", err
 	}
 
-	if latest > current && !isNew {
-		return l.createBackup()
+	// Not create backup if new database, or schema up to date
+	if latest <= current || isNew {
+		return "", nil // Consider as graceful return
 	}
 
-	return "", nil // Consider as graceful return
+	// Prepare database name
+	dest = defaultBackupPath(l.dbPath, l.backupDir)
+
+	// Backup
+	err = l.BackupTo(dest)
+	if err != nil {
+		return "", err
+	}
+
+	return dest, nil
 }
 
 // Get default absolute path to backup database.
